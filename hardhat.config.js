80a3d4353f24c19fa2942d4ea7b4d5ee7d80690c
rasadregmi <regmirasad53@gmail.com>
2025-07-21 19:00:51 +0545

    Add hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+      viaIR: true,
+    },
+  },
+  networks: {
+    localhost: {
+      url: process.env.LOCALHOST_RPC_URL || "http://127.0.0.1:8545",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+}; 
