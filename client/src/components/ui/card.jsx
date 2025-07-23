423eabec1226ed97412a77e0eca07c05c335e4e1
rasadregmi <regmirasad53@gmail.com>
2025-07-23 08:24:53 +0545

    Update client/src/components/ui/card.jsx

--- client/src/components/ui/card.jsx
+++ client/src/components/ui/card.jsx
@@ -1,9 +1,9 @@
 import { cn } from '../../lib/utils';
 
 export function Card({ children, className = "" }) {
-  return <div className={cn("bg-white border border-border rounded-2xl shadow-lg p-6 transition-shadow hover:shadow-2xl", className)}>{children}</div>;
+  return <div className={cn("bg-white border border-border rounded-2xl shadow-lg p-3 transition-shadow hover:shadow-2xl", className)}>{children}</div>;
 }
 
 export function CardContent({ children, className = "" }) {
-  return <div className={cn("py-2 px-1", className)}>{children}</div>;
+  return <div className={cn("py-1 px-1", className)}>{children}</div>;
 }
